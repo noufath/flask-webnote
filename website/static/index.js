c2d9f96454a7964c7e3dd67db89c43d5dad4389b
noufath <danychristianto@gmail.com>
2021-06-28 20:29:03 +0700

    add javascript to remote record

---
+++ website/static/index.js
@@ -0,0 +1,8 @@
+function deleteNote(noteID) {
+    fetch("/delete-note", {
+        method: "POST",
+        body: JSON.stringify({ noteId: noteID }),
+    }).then((_res) => {
+        window.location.href = "/";
+    });
+}
